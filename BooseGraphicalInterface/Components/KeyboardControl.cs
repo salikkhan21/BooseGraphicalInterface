b30e6207cff12ffca095ae231742d5104e190e2d
Salik Raj <Salikraja50@gmail.com>
2024-11-15 00:18:06 +0500

    Add project files.

---
+++ BooseGraphicalInterface/Components/KeyboardControl.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace BooseGraphicalInterface
+{
+    /// <summary>
+    /// A custom user control for a virtual keyboard.
+    /// </summary>
+    public partial class KeyboardControl : UserControl
+    {
+        private TextBox? activeTextBox; // The currently active TextBox
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyboardControl"/> class.
+        /// </summary>
+        public KeyboardControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets the active TextBox that will receive keyboard input.
+        /// </summary>
+        /// <param name="textBox">The TextBox to set as active.</param>
+        public void SetActiveTextBox(TextBox textBox)
+        {
+            activeTextBox = textBox;
+        }
+
+        private void InitializeComponent()
+        {
+            int buttonWidth = 75;
+            int buttonHeight = 40;
+            int buttonSpacing = 5;
+
+            // Create and configure the FlowLayoutPanels for button arrangement
+            var buttonLayouts = new FlowLayoutPanel[5];
+            string[] rowContents = { "QWERTYUIOP[]", "ASDFGHJKL;'", "ZXCVBNM,./()", "1234567890", "-+=*/" };
+
+            for (int i = 0; i < 5; i++)
+            {
+                buttonLayouts[i] = CreateButtonLayout(rowContents[i], buttonWidth, buttonHeight);
+                buttonLayouts[i].Location = new System.Drawing.Point(0, 10 + i * (buttonHeight + buttonSpacing));
+                this.Controls.Add(buttonLayouts[i]);
+            }
+
+            // Set the size of the control
+            this.Size = new System.Drawing.Size(830, 300);
+
+            // Add the space, enter, and backspace buttons on the last row
+            var spaceButton = CreateButton("Space", 200, buttonHeight);
+            var enterButton = CreateButton("Enter", 100, buttonHeight);
+            var backspaceButton = CreateButton("Backspace", 100, buttonHeight);
+
+            var lastButtonLayout = buttonLayouts.Last();
+            lastButtonLayout.Controls.Add(spaceButton);
+            lastButtonLayout.Controls.Add(enterButton);
+            lastButtonLayout.Controls.Add(backspaceButton);
+
+            // Expand the last row with the space, enter, and backspace buttons
+            lastButtonLayout.Size = new System.Drawing.Size(lastButtonLayout.Size.Width + 400, lastButtonLayout.Size.Height);
+        }
+
+        private FlowLayoutPanel CreateButtonLayout(string rowContent, int buttonWidth, int buttonHeight)
+        {
+            var buttonLayout = new FlowLayoutPanel
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                WrapContents = true,
+                Size = new System.Drawing.Size(buttonWidth * rowContent.Length, buttonHeight + 5)
+            };
+
+            foreach (char key in rowContent)
+            {
+                var button = CreateButton(key.ToString(), 62, buttonHeight);
+                buttonLayout.Controls.Add(button);
+            }
+
+            return buttonLayout;
+        }
+
+        /// <summary>
+        /// Creates a button with the specified text and size.
+        /// </summary>
+        /// <param name="text">The text to display on the button.</param>
+        /// <param name="width">The width of the button.</param>
+        /// <param name="height">The height of the button.</param>
+        /// <returns>The created button.</returns>
+        private Button CreateButton(string text, int width, int height)
+        {
+            var button = new Button
+            {
+                Size = new System.Drawing.Size(width, height),
+                Text = text,
+            };
+
+            button.Click += (sender, e) =>
+            {
+                if (activeTextBox != null)
+                {
+                    string inputText = button.Text;
+
+                    if (inputText == "Space")
+                    {
+                        inputText = " ";
+                    }
+                    else if (inputText == "Enter")
+                    {
+                        inputText = Environment.NewLine;
+                    }
+                    else if (inputText == "Backspace")
+                    {
+                        if (activeTextBox.Text.Length > 0)
+                        {
+                            activeTextBox.Text = activeTextBox.Text.Substring(0, activeTextBox.Text.Length - 1);
+                        }
+                        return;
+                    }
+
+                    activeTextBox.Focus();
+                    SendKeys.SendWait(inputText);
+                }
+            };
+
+            return button;
+        }
+    }
+}
---
+++ BooseGraphicalInterface/Program.cs
@@ -0,0 +1,133 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace BooseGraphicalInterface
+{
+    /// <summary>
+    /// The main form of the Graphical Programming Language application.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        private TextBox programTextBox = new TextBox();
+        private TextBox commandTextBox = new TextBox();
+        private Button runButton = new Button();
+        private Button syntaxCheckButton = new Button();
+        private PictureBox resultBox = new PictureBox();
+        private Graphics resultBoxGraphics;
+        private KeyboardControl keyboardControl = new KeyboardControl();
+        private Label statusLabel = new Label();
+        private Button saveButton = new Button();
+        private Button loadButton = new Button();
+        private Button openNewThread = new Button();
+        private List<Form> additionalForms = new List<Form>();
+
+       
+        public MainForm()
+        {
+            InitializeComponent();
+            this.resultBoxGraphics = resultBox.CreateGraphics();
+            
+        }
+
+        /// <summary>
+        /// Initializes the GUI components.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // Create and configure GUI components
+            programTextBox.Multiline = true;
+            programTextBox.Size = new System.Drawing.Size(400, 200);
+            programTextBox.Location = new System.Drawing.Point(10, 10);
+            programTextBox.ScrollBars = ScrollBars.Vertical;
+
+            // Set up the keyboard control
+            programTextBox.Enter += (sender, e) =>
+            {
+                keyboardControl.SetActiveTextBox(programTextBox);
+            };
+
+            commandTextBox.Size = new System.Drawing.Size(400, 30);
+            commandTextBox.Location = new System.Drawing.Point(10, 220);
+
+            commandTextBox.Enter += (sender, e) =>
+            {
+                keyboardControl.SetActiveTextBox(commandTextBox);
+            };
+
+            // add different buttons and their properties
+            runButton.Text = "Run";
+            runButton.Size = new System.Drawing.Size(195, 30);
+            runButton.Location = new System.Drawing.Point(10, 260);
+           
+
+            syntaxCheckButton.Text = "Syntax Check";
+            syntaxCheckButton.Size = new System.Drawing.Size(195, 30);
+            syntaxCheckButton.Location = new System.Drawing.Point(215, 260);
+           
+
+            saveButton.Text = "Save";
+            saveButton.Size = new System.Drawing.Size(195, 30);
+            saveButton.Location = new System.Drawing.Point(10, 300);
+           
+
+            loadButton.Text = "Load";
+            loadButton.Size = new System.Drawing.Size(195, 30);
+            loadButton.Location = new System.Drawing.Point(215, 300);
+           
+            openNewThread.Text = "Open New Thread";
+            openNewThread.Size = new System.Drawing.Size(195, 30);
+            openNewThread.Location = new System.Drawing.Point(215, 340);
+
+           
+
+            resultBox.Size = new System.Drawing.Size(400, 400);
+            resultBox.Location = new System.Drawing.Point(430, 10);
+            resultBox.BackColor = System.Drawing.Color.LightGray;
+            this.Controls.Add(resultBox);
+
+            // info label
+            statusLabel.Text = "Position: {X=0, Y=0}\nFill: ON\nColor: Red";
+            statusLabel.Size = new System.Drawing.Size(400, 50);
+            statusLabel.Location = new System.Drawing.Point(10, 340);
+
+            // Set up the form
+            this.Text = "BOOSE GUI";
+            this.Size = new System.Drawing.Size(855, 700);
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Controls.Add(programTextBox);
+            this.Controls.Add(commandTextBox);
+            this.Controls.Add(runButton);
+            this.Controls.Add(syntaxCheckButton);
+            this.Controls.Add(saveButton);
+            this.Controls.Add(loadButton);
+            this.Controls.Add(openNewThread);
+            this.Controls.Add(resultBox);
+            this.Controls.Add(statusLabel);
+
+            keyboardControl = new KeyboardControl();
+            this.Controls.Add(keyboardControl);
+            keyboardControl.BringToFront();
+            keyboardControl.Location = new System.Drawing.Point(10, 420);
+            keyboardControl.Visible = true;
+        }
+
+      
+
+     
+      
+
+     
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
